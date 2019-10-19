1a0a5c72de3de9fe16ea36679ad8672792f12667
Maika Kailani <queenmab051@gmail.com>
2019-10-19 20:05:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,3 +13,10 @@ def link
   puts phrase
 end
 
+describe "any_phrase" do
+  it "takes an argument and puts out the catch phrase" do
+    phrase = "Do a barrel roll!"
+    expect{any_phrase(phrase)}.to output("Do a barrel roll!\n).to_stdout
+  end
+end
+  
